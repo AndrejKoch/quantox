919a6806c8b49f07e8eef37b440136ed1c2e05a2
Andrej <kochevskiak@gmail.com>
2021-03-09 20:34:35 +0100

    Create Application and Router, enable autoloading(namespace) with composer and implement routing

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '03eb74d6f2af820f7532c46921de600f34a0b1cf',
+    'name' => 'vagrant/quantox',
+  ),
+  'versions' => 
+  array (
+    'vagrant/quantox' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '03eb74d6f2af820f7532c46921de600f34a0b1cf',
+    ),
+  ),
+);
